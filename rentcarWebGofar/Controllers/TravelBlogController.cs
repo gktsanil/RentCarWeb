1ff86a0aedd722269ec31d7cfb67f3fcc1010ab9
Anıl Goktas <anilgkts@outlook.com.tr>
2018-05-19 23:03:16 +0300

    update
    update

---
+++ rentcarWebGofar/Controllers/TravelBlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rentcarWebGofar.Controllers
+{
+    public class TravelBlogController : Controller
+    {
+        // GET: TravelBlog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: TravelBlog Detail
+        public ActionResult BlogDetail()
+        {
+            return View();
+        }
+    }
+}
