5683c7f0eb82c28ec7effe65a4a3b3953e23d06b
Anıl Goktas <anilgkts@outlook.com.tr>
2018-05-19 21:32:03 +0300

    newUpdate
    update all project

---
+++ rentcarWebGofar/Controllers/DestinationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rentcarWebGofar.Controllers
+{
+    public class DestinationController : Controller
+    {
+        // GET: Destination
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Destination
+        public ActionResult GridIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ rentcarWebGofar/Controllers/TripsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rentcarWebGofar.Controllers
+{
+    public class TripsController : Controller
+    {
+        // GET: Trips
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: TripDetail
+        public ActionResult TripDetail()
+        {
+            return View();
+        }
+    }
+}
