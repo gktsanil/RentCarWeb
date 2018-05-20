fc039fba6bdc7aa48a94e5ab7ed41b791786ab94
Anıl Goktas <anilgkts@outlook.com.tr>
2018-05-20 09:45:09 +0300

    add database
    update and add database with entity freamworks

--- rentcarWebGofar/DBModel/Context/ProjectContext.cs
+++ rentcarWebGofar/DBModel/Context/ProjectContext.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using rentcarWebGofar.DBModel.Entity;
 
 namespace rentcarWebGofar.DBModel.Context
 {
-    public class ProjectContext
+    public class ProjectContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
     }
 }
--- rentcarWebGofar/DBModel/Entity/User.cs
+++ rentcarWebGofar/DBModel/Entity/User.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace rentcarWebGofar.DBModel.Entity
 {
     public class User
     {
+        [Key]
+        public string UserID { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserSurName { get; set; }
+        public string UserPassword { get; set; }
+        public Boolean UserType { get; set; }
+
+
+        /*public virtual List<CustomerCustomerDemo> CustomerCustomerDemos { get; set; }
+
+        public CustomerDemographic()
+        {
+            this.CustomerCustomerDemos = new List<CustomerCustomerDemo>();
+        }
+        */
     }
 }
