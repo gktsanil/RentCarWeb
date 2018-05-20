93fe14ae2a1feededc815b64c1ad786f397aa975
Anıl Goktas <anilgkts@outlook.com.tr>
2018-05-20 16:20:25 +0300

    fix and update
    fix homepage errors

---
+++ rentcarWebGofar/Controllers/RentCarController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rentcarWebGofar.Controllers
+{
+    public class RentCarController : Controller
+    {
+        // GET: RentCar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: RentCar Detail
+        public ActionResult RentCarDetail()
+        {
+            return View();
+        }
+    }
+}
