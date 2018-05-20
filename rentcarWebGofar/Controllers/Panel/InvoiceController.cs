99ddabf4313bb2b815ad7faac93d169184b6e873
Anıl Goktas <anilgkts@outlook.com.tr>
2018-05-20 15:58:28 +0300

    update panel
    update panel and add new component

---
+++ rentcarWebGofar/Controllers/Panel/InvoiceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rentcarWebGofar.Controllers.Panel
+{
+    public class InvoiceController : Controller
+    {
+        // GET: Invoice Incoming
+        public ActionResult Incoming()
+        {
+            return View();
+        }
+        
+        // GET: Invoice Outgoing
+        public ActionResult Outgoing()
+        {
+            return View();
+        }
+        
+        // GET: Invoice Canceled
+        public ActionResult Canceled()
+        {
+            return View();
+        }
+        
+        // GET: Invoice Page
+        public ActionResult ShowInvoice()
+        {
+            return View();
+        }
+    }
+}
